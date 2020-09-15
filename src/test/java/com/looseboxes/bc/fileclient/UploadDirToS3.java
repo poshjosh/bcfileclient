f8bf678835b3d6bdd34f3e82015269c1b75c0fa4
poshjosh <posh.bc@gmail.com>
2020-09-15 17:34:29 +0100

    Update README

--- src/test/java/com/looseboxes/bc/fileclient/UploadDirToS3.java
+++ src/test/java/com/looseboxes/bc/fileclient/UploadDirToS3.java
@@ -5,7 +5,6 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.looseboxes.fileclient.Utils;
 import com.looseboxes.fileclient.aws.AwsFileHandler;
-import com.looseboxes.fileclient.aws.AwsFileHandler.S3FileKeyBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -17,25 +16,39 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author hp
  */
 public class UploadDirToS3 {
-
+    
+    public static void main(String... args) {
+        String bucketName = "???";
+        String accessKey = "???";
+        String secretKey = "???";
+        Path base = Paths.get(System.getProperty("user.home"));
+        Path context = Paths.get(".webstore", "config");
+        Path dirToUpload = base.resolve(context);
+        Path s3FileKeyPrefix = context;
+        int maxDepth = 7;
+        new UploadDirToS3().upload(bucketName, accessKey, secretKey, dirToUpload, s3FileKeyPrefix, maxDepth);
+    }
+    
     public void upload(
-            String s3BucketName, String s3ObjectKeyPrefix,
-            String awsAccessKey, String awsSecretKey, 
-            Path dirToUploadToS3, int maxDepth) {
+            String s3BucketName, String awsAccessKey, String awsSecretKey, 
+            Path dirToUploadToS3, Path s3FileKeyPrefix, int maxDepth) {
     
         AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
         
         AmazonS3Client s3Client = new AmazonS3Client(awsCredentials); 
      
-        S3FileKeyBuilder s3FileKeyBuilder = (path) -> {
+        AwsFileHandler.S3FileKeyBuilder s3FileKeyBuilder = (path) -> {
             
-            String relative = dirToUploadToS3.relativize(path).toString();
+            Path relativePath = dirToUploadToS3.relativize(path);
             
-            Path s3FileKey = Paths.get(s3ObjectKeyPrefix, relative);
+            String relative = s3FileKeyPrefix == null ? relativePath.toString() :
+                    s3FileKeyPrefix.resolve(relativePath).toString();
+            
+            String s3FileKey = relative.replace('\\', '/');
             
             System.out.println("Path: " + path + ", S3FileKey: " + s3FileKey);
             
-            return s3FileKey.toString().replace('\\', '/');
+            return s3FileKey;
         };
         
         AwsFileHandler fileHandler = new AwsFileHandler(s3BucketName, s3Client, s3FileKeyBuilder);
